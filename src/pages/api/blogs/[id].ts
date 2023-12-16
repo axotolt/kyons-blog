cd647df3e7892aa0afce7e91c257640309f3f69d
axotolt <dung220802@gmail.com>
2023-12-16 10:02:42 +0700

    add title duplication check

--- src/pages/api/blogs/[id].ts
+++ src/pages/api/blogs/[id].ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import { app } from "@lib/firebase/server";
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore} from "firebase-admin/firestore";
 
 const db = getFirestore(app);
 const blogsRef = db.collection("blogs");
@@ -8,12 +8,16 @@ const blogsRef = db.collection("blogs");
 export const POST: APIRoute = async ({ params, redirect, request }) => {
   const formData = await request.formData();
   const title = formData.get("title")?.toString();
-  const tags = formData.get("tags")?.toString();
+  const labels = formData.get("labels")?.toString();
   const author = formData.get("author")?.toString();
   const authorid = formData.get("authorid")?.toString();
   const content = formData.get("content")?.toString();
+  const changeHistory = {
+    changer: author,
+    changerid: authorid,
+    updatedat: new Date().toISOString(),
+  };
   
-
   if (!title || !author) {
     return new Response("Missing required fields", {
       status: 400,
@@ -27,12 +31,15 @@ export const POST: APIRoute = async ({ params, redirect, request }) => {
   }
 
   try {
+    const currentBlog = await blogsRef.doc(params.id).get();
+    const currentData = currentBlog.data();
     await blogsRef.doc(params.id).update({
       title,
-      tags,
-      author,
-      authorid,
+      labels,
+      // author,
+      // authorid,
       content,
+      changeHistory: [...(currentData?.changeHistory || []), changeHistory],
     });
   } catch (error) {
     return new Response("Something went wrong", {
--- src/pages/api/blogs/index.ts
+++ src/pages/api/blogs/index.ts
@@ -2,14 +2,30 @@ import type { APIRoute } from "astro";
 import { app } from "@lib/firebase/server";
 import { getFirestore } from "firebase-admin/firestore";
 
+// const db = getFirestore(app);
+// const blogsRef = db.collection("blogs");
+// const getAllTitles = async () => {
+//   const querySnapshot = await blogsRef.get();
+//   const titles = [];
+//   querySnapshot.forEach((doc) => {
+//     titles.push(doc.data().title);
+//   });
+//   return titles;
+// };
+// const allTitles = await getAllTitles();
+
 export const POST: APIRoute = async ({ params, redirect, request }) => {
   const formData = await request.formData();
   const title = formData.get("title")?.toString();
-  const tags = formData.get("tags")?.toString();
+  const labels = formData.get("labels")?.toString();
   const author = formData.get("author")?.toString();
   const authorid = formData.get("authorid")?.toString(); 
   const content = formData.get("content")?.toString();
+  const createdAt = new Date().toISOString();
 
+  // if (allTitles.includes(title)) {
+    
+  // }
   if (!title || !author) {
     return new Response("Missing required fields", {
       status: 400,
@@ -20,10 +36,11 @@ export const POST: APIRoute = async ({ params, redirect, request }) => {
     const blogsRef = db.collection("blogs");
     await blogsRef.add({
       title,
-      tags,
+      labels,
       author,
       authorid,
       content,
+      createdAt
     });
   } catch (error) {
     return new Response("Something went wrong", {
